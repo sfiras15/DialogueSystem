fe3eaa79cf4b27fda22951ffbeedd94d6caf2197
Firas Sediri <sfiras1111@gmail.com>
2023-07-14 14:12:05 +0100

    Adding files

---
+++ Assets/NodeBasedDialogueSystem-master/com.subtegral.dialoguesystem/Editor/Graph/NodeSearchWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Subtegral.DialogueSystem.Editor
+{
+    public class NodeSearchWindow : ScriptableObject,ISearchWindowProvider
+    {
+        private EditorWindow window;
+        private StoryGraphView graphView;
+
+        private Texture2D indentationIcon;
+        
+        public void Configure(EditorWindow window,StoryGraphView graphView)
+        {
+            this.window = window;
+            this.graphView = graphView; 
+            //Transparent 1px indentation icon as a hack
+            indentationIcon = new Texture2D(1,1);
+            indentationIcon.SetPixel(0,0,new Color(0,0,0,0));
+            indentationIcon.Apply();
+        }
+        
+        public List<SearchTreeEntry> CreateSearchTree(SearchWindowContext context)
+        {
+            var tree = new List<SearchTreeEntry>
+            {
+                new SearchTreeGroupEntry(new GUIContent("Create Node"), 0),
+                new SearchTreeGroupEntry(new GUIContent("Dialogue"), 1),
+                new SearchTreeEntry(new GUIContent("Dialogue Node", indentationIcon))
+                {
+                    level = 2, userData = new DialogueNode()
+                },
+                //Add other types of nodes here
+            };
+
+            return tree;
+        }
+
+        public bool OnSelectEntry(SearchTreeEntry SearchTreeEntry, SearchWindowContext context)
+        {
+            //Editor window-based mouse position
+            var mousePosition = window.rootVisualElement.ChangeCoordinatesTo(window.rootVisualElement.parent,
+                context.screenMousePosition - window.position.position);
+            var graphMousePosition = graphView.contentViewContainer.WorldToLocal(mousePosition);
+            switch (SearchTreeEntry.userData)
+            {
+                case DialogueNode dialogueNode:
+                    graphView.CreateNewDialogueNode("Dialogue Node",graphMousePosition);
+                    return true;
+                // and here 
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/NodeBasedDialogueSystem-master/com.subtegral.dialoguesystem/Editor/Graph/StoryGraph.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Subtegral.DialogueSystem.DataContainers;
+
+namespace Subtegral.DialogueSystem.Editor
+{
+    public class StoryGraph : EditorWindow
+    {
+        private string fileName = "New Narrative";
+
+        private StoryGraphView graphView;
+        [MenuItem("Graph/Narrative Graph")]
+        public static void CreateGraphViewWindow()
+        {
+            var window = GetWindow<StoryGraph>();
+            window.titleContent = new GUIContent("Narrative Graph");
+        }
+
+        private void OnEnable()
+        {
+            ConstructGraphView();
+            GenerateToolbar();
+        }
+
+        private void ConstructGraphView()
+        {
+            graphView = new StoryGraphView(this)
+            {
+                name = "Narrative Graph",
+            };
+            graphView.StretchToParentSize();
+            rootVisualElement.Add(graphView);
+        }
+
+        private void GenerateToolbar()
+        {
+            var toolbar = new Toolbar();
+            var fileNameTextField = new TextField("File Name:");
+
+            fileNameTextField.SetValueWithoutNotify(fileName);
+            fileNameTextField.MarkDirtyRepaint();
+            fileNameTextField.RegisterValueChangedCallback(evt => fileName = evt.newValue);
+
+            toolbar.Add(fileNameTextField);
+            toolbar.Add(new Button(() => RequestDataOperation(true)) {text = "Save Data"});
+            toolbar.Add(new Button(() => RequestDataOperation(false)) {text = "Load Data"});
+            rootVisualElement.Add(toolbar);
+        }
+
+        private void RequestDataOperation(bool save)
+        {
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                var saveUtility = GraphSaveUtility.GetInstance(graphView);
+                if (save)
+                    saveUtility.SaveGraph(fileName);
+                else
+                    saveUtility.LoadNarrative(fileName);
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Invalid File name", "Please Enter a valid filename", "OK");
+            }
+        }
+
+        private void OnDisable()
+        {
+            rootVisualElement.Remove(graphView);
+        }
+    }
+}
